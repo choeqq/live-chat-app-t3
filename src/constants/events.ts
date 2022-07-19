7c8c82c7953db332fe9fef901bcd6c2746a91fc0
choeqq <k.chola@op.pl>
2022-07-19 21:13:13 +0200

    server running, schemas added

---
+++ src/constants/events.ts
@@ -0,0 +1,3 @@
+export enum Events {
+  SENT_MESSAGE = "SEND_MESSAGE",
+}
--- src/server/wsServer.ts
+++ src/server/wsServer.ts
@@ -7,6 +7,19 @@ const wss = new ws.Server({
   port: 3001,
 });
 
+const handler = applyWSSHandler({ wss, router: appRouter, createContext });
+
 wss.on("connection", () => {
-  console.log(`++ ws connection ${wss.clients.size}`);
+  console.log(`Got a connection ${wss.clients.size}`);
+  wss.once("close", () => {
+    console.log(`Closed connection ${wss.clients.size}`);
+  });
+});
+
+console.log(`wss server start at ws://localhost:3001`);
+
+process.on("SIGTERM", () => {
+  console.log("Got SIGTERM");
+  handler.broadcastReconnectNotification();
+  wss.close();
 });
